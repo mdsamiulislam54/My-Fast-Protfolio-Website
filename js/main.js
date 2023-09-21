1a7dfb247b0f0d33931f4847afdc7b541b555292
mdsamiulislam54 <samiulm5332@gmail.com>
2023-09-21 16:42:34 +0600

    my fast protfolio website

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+	$('nav-ber').meanMenu({
+        meanMenuContainer:'.mobil-area',
+        meanScreenWidth:480,
+    });
+});
---
+++ meanMenu-master/meanMenu-master/main.js
@@ -0,0 +1,7 @@
+jquery(document).ready(function () {
+    jquery('.row') .header-area();
+	jquery('nav-ber').meanMenu({
+        meanMenuContainer:'.mobil-area',
+        meanScreenWidth:480,
+    });
+});
